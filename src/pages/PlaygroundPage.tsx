38d4ca45e0b18164198565c8d30b3be8f34baa71
Usama Bhakrani <usamabhakrani@gmail.com>
2025-02-10 21:35:39 +0500

    ExpandableText test finalized

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cupiditate! Soluta sed distinctio ea consequatur, earum dicta dolore, modi dolorum illum cumque accusantium error necessitatibus quia neque eveniet commodi facere qui minus possimus explicabo quis dolor voluptates tempora ad. Deserunt assumenda cupiditate incidunt sunt aspernatur tempora ut quisquam, iusto amet officia quo aperiam illo, accusantium quibusdam sapiente eligendi. Necessitatibus quia dolorum quibusdam eos ipsam obcaecati delectus a iusto esse, minima dignissimos, quos eaque ipsa id soluta nulla sed magnam dicta placeat dolore at reiciendis, eveniet neque! Tempora totam sapiente iure excepturi illum, fugit recusandae neque officia quas fugiat veritatis rerum!" />
+  );
 };
 
 export default PlaygroundPage;
